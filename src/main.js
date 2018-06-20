1e8ff0f6b939791fdc762747a44006442b74c63a
Clement Procureur <clement@sgrx04.afp.local>
2018-06-20 10:58:32 +0200

    better webpack and select flow

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+import ElementUI from 'element-ui'
 
-Vue.use(ElementUI);
+Vue.use(ElementUI)
+
+Vue.config.devtools = true
 
 new Vue({
-    el: '#app',
-    render: createElement => createElement(App)
-});
+  el: '#app',
+  render: h => h(App)
+})
---
+++ src/views/clients/SelectClient.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- <el-select
+    v-bind:value="clientId"
+    @input="$emit('input', $event)"
+    placeholder="Filter by short name"
+    filterable="true"
+    remote="true"
+    :remote-method="filter"
+    :loading="loading">
+    <el-option
+      v-for="item in clients"
+      :key="item._id"
+      :label="item.name"
+      :value="item._id">
+    </el-option>
+  </el-select> -->
+
+  <el-select
+    :value="selected"
+    @input="dispatch"
+    placeholder="Filter by short name"
+    :filterable="true"
+    :remote="true"
+    :remote-method="filter"
+    :loading="loading">
+    <el-option
+      v-for="item in clients"
+      :key="item._id"
+      :label="item.name"
+      :value="item._id">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+export default {
+  name: 'SelectClient',
+
+  data() {
+    return {
+      selected: '',
+      clients: [],
+      loading: false,
+    }
+  },
+
+  methods: {
+    filter(query) {
+      if (query !== '') {
+        this.loading = true;
+        setTimeout(() => {
+          this.loading = false
+          this.clients = [{_id: '1', name: 'foo'}, {_id: '2', name: 'bar'}]
+        }, 200)
+      } else {
+        this.clients = []
+      }
+    },
+
+    dispatch (e) {
+      console.log(e)
+      this.$emit('input', e)
+    }
+  }
+}
+</script>
